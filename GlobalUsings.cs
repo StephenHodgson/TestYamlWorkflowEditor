0425e6fd6f90e1160b5c65a188e1a963f672d8ff
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-09-20 09:23:35 -0400

    .

---
+++ GlobalUsings.cs
@@ -0,0 +1,6 @@
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+global using System.Dynamic;
+global using System.Text.Encodings.Web;
+global using System.Text.Json;
+global using VYaml.Serialization;
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Overview: This console app is designed to read a GitHub Actions Workflow YAML file
+// and edit the file depending on the user's inputs.
+
+Console.WriteLine("Enter the path to the YAML file:");
+var inputPath = Console.ReadLine();
+
+// check that path is valid and file exists
+if (!File.Exists(inputPath))
+{
+    Console.WriteLine("File does not exist");
+    return;
+}
+// open the file and read the contents as utf8 bytes
+var yamlUtf8Bytes = await File.ReadAllBytesAsync(inputPath);
+var yamlObject = YamlSerializer.Deserialize<dynamic>(yamlUtf8Bytes);
+Console.WriteLine("YAML file loaded successfully");
+
+var json = JsonSerializer.Serialize(yamlObject, new JsonSerializerOptions
+{
+    WriteIndented = true,
+    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+});
+Console.WriteLine(json);
+// modify the yamlObject workflow name property to TEST
+yamlObject["name"] = "TEST";
+
+// After modifying the yamlObject, serialize and write it back to the same input file
+ReadOnlyMemory<byte> modifiedYamlUtf8Bytes = YamlSerializer.Serialize(yamlObject);
+await File.WriteAllBytesAsync(inputPath, modifiedYamlUtf8Bytes.ToArray());
+Console.WriteLine("YAML file modified successfully");
